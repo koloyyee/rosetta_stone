5988f4666ce574e5c7f3c5f1beb97b9aaef074df
Loy Yee Ko <koloyyee@gmail.com>
2025-03-31 23:27:26 -0400

    moved

--- automation/filewatcher/js/index.js
+++
@@ -1,3 +0,0 @@
-/** @file File watcher */
-
-
---
+++ automation/java/filewatcher/src/main/java/co/loyyee/Main.java
@@ -0,0 +1,7 @@
+package co.loyyee;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
